55af824e602b6240468cacbf0f99feee24b9cea6
slendchat <slendchat@gmail.com>
2025-10-06 17:16:37 +0300

    readme added

--- lab03/boids/config.py
+++ lab03/boids/config.py
@@ -2,20 +2,20 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-  SCREEN_WIDTH:int = 800
-  SCREEN_HEIGHT:int = 600
+  SCREEN_WIDTH:int = 1600
+  SCREEN_HEIGHT:int = 900
 
   MAX_FPS:int = 60
 
-  weight_separation:float = 1.5
-  weight_alignment:float = 1.0
-  weight_coherence:float = 1.5
+  weight_separation:float = 1.3
+  weight_alignment:float = 1.4
+  weight_coherence:float = 1.2
 
-  view_radius: float = 80.0
+  view_radius: float = 150.0
 
-  max_force: float = 60.0
-  max_speed: float = 250.0
-  min_speed: float = 60.0
+  max_force: float = 80.0
+  max_speed: float = 360.0
+  min_speed: float = 120.0
   friction: float = 0.98
 
   EPS = 1e-6
