f6389fd569b6118b17d0880099cf9c70d2a226e2
slendchat <slendchat@gmail.com>
2025-10-07 08:51:49 +0300

    added screenshots and changed board size

--- lab04/inc/board.h
+++ lab04/inc/board.h
@@ -1,8 +1,8 @@
 #pragma once
 #include "../inc/cell.h"
 
-#define height_y 450
-#define width_x 1250
+#define height_y 100
+#define width_x 250
 
 extern cell_entity main_board[height_y][width_x];
 extern cell_entity buffer_board[height_y][width_x];
